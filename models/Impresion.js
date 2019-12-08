a048cd431af39da0fb3ad8cfd05c88ddaaa77eee
MarcosSorto <marcoselisorto.me@hotmail.com>
2019-12-08 11:57:38 -0600

    Creación del modelo impresión

---
+++ models/Impresion.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const shortid = require("shortid");
+const slug = require("slug");
+const Schemma = mongoose.Schema;
+
+const impresionSchemma = new Schemma({
+  documento: {
+    type: String,
+    trim: true
+  },
+  color: {
+    type: String,
+    trim: true
+  },
+  tamanio: {
+    type: String,
+    trim: true
+  },
+  tipoHoja: {
+    type: String,
+    trim: true,
+    default: "Carta"
+  },
+  cantidadHojas: {
+    type: Number,
+    default: 0
+  },
+  especificaciones: {
+    type: String
+  },
+  usuario: {
+    type: Schema.objectId,
+    ref: "Usuario"
+  },
+  estado: {
+    type: Number,
+    default: 1
+  },
+  url: {
+    type: String
+  },
+  fechaEntrega: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Middleware para generar el url
+impresionSchemma.pre("save", function(next) {
+  //creamos la url
+  const url = slug(this.documento);
+  this.url = `${url}-${shortid.generate()}`;
+
+  next();
+});
+
+// Middleware para crear url
+usuarioSchema.pre("save", function(next) {
+  // Crear la URL
+  const url = slug(this.nombre);
+  this.url = `${url}-${shortid.generate()}`;
+  //console.log(this.url);
+
+  next();
+});
